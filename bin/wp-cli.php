7796c824eb0f1cc8aa087213cbf1f92e679031c9
Norman Kämper-Leymann <norman.leymann@gmail.com>
2018-09-03 14:29:14 +0200

    Documentation.

--- bin/wp-cli.php
+++ bin/wp-cli.php
@@ -24,14 +24,12 @@ else {
   exit(1);
 }
 
-$DRUSH_LAUNCHER_VERSION = '@git-version@';
+$LAUNCHER_VERSION = '@git-version@';
 
-$VERSION = FALSE;
-
+$VERSION_CHECK = FALSE;
 foreach ($_SERVER['argv'] as $arg) {
-  // If a variable to set was indicated on the
-  // previous iteration, then set the value of
-  // the named variable (e.g. "ROOT") to "$arg".
+  // If a variable to set was indicated on the previous iteration,
+  // then set the value of the named variable (e.g. "ROOT") to "$arg".
   if ($VAR) {
     $$VAR = "$arg";
     $VAR = FALSE;
@@ -39,7 +37,7 @@ foreach ($_SERVER['argv'] as $arg) {
   else {
     switch ($arg) {
       case "--version":
-        $VERSION = TRUE;
+        $VERSION_CHECK = TRUE;
         break;
     }
   }
@@ -47,41 +45,49 @@ foreach ($_SERVER['argv'] as $arg) {
 
 $ROOT = getcwd();
 
-$drupalFinder = new WordpressFinder();
+$wordpressFinder = new WordpressFinder();
 
-if ($VERSION) {
-  echo "Drush Launcher Version: {$DRUSH_LAUNCHER_VERSION}" . PHP_EOL;
+if ($VERSION_CHECK) {
+  echo "Drush Launcher Version: {$LAUNCHER_VERSION}" . PHP_EOL;
   exit(0);
 }
 
-if ($drupalFinder->locateRoot($ROOT)) {
-  $drupalRoot = $drupalFinder->getWebRoot();
+if ($wordpressFinder->locateRoot($ROOT)) {
+  $webRoot = $wordpressFinder->getWebRoot();
 
-  // Detect WP-CLI version
-  if (file_exists($drupalFinder->getVendorDir() . '/wp-cli/wp-cli/VERSION')) {
-    $version_file = $drupalFinder->getVendorDir() . '/wp-cli/wp-cli/VERSION';
-    $DRUSH_VERSION = file_get_contents($version_file);
-    $DRUSH_VERSION = str_replace(["\r", "\n"], '', $DRUSH_VERSION);
+  // Detect WP-CLI version.
+  if (file_exists($wordpressFinder->getVendorDir() . '/wp-cli/wp-cli/VERSION')) {
+    $version_file = $wordpressFinder->getVendorDir() . '/wp-cli/wp-cli/VERSION';
+    $WP_CLI_VERSION = file_get_contents($version_file);
+    $WP_CLI_VERSION = str_replace(["\r", "\n"], '', $WP_CLI_VERSION);
   }
 
-  if ($DRUSH_VERSION == '2.0.1') {
-    chdir($drupalRoot);
-    require_once $drupalFinder->getVendorDir() . '/wp-cli/wp-cli/php/boot-fs.php';
   // For now WP-CLI version must be anywhere between 2.0.0 and 3.0.0.
   if (gettype($WP_CLI_VERSION) == 'string' && version_compare($WP_CLI_VERSION, '2.0.0') && version_compare('3.0.0', $WP_CLI_VERSION, '<=')) {
+
+    // We need to be in the WordPress directory to fire the command.
+    // Drush Launcher does this via Drush's `drush_set_option('root', $webRoot);`.
+    // Maybe WP-CLI has something similar.
+    chdir($webRoot);
+
+    // Fire command.
+    require_once $wordpressFinder->getVendorDir() . '/wp-cli/wp-cli/php/boot-fs.php';
+
+    // And change back.
     chdir($ROOT);
+
     exit(0);
   }
 
-  if (!$DRUSH_VERSION) {
-    echo 'The Drush launcher could not find a local Drush in your Drupal site.' . PHP_EOL;
-    echo 'Please add Drush with Composer to your project.' . PHP_EOL;
-    echo 'Run \'cd "' . $drupalFinder->getComposerRoot() . '" && composer require wp-cli/wp-cli\'' . PHP_EOL;
+  if (!$WP_CLI_VERSION) {
+    echo 'The WP-CLI Launcher could not find a local WP-CLI in your WordPress site.' . PHP_EOL;
+    echo 'Please add WP-CLI with Composer to your project.' . PHP_EOL;
+    echo 'Run \'cd "' . $wordpressFinder->getComposerRoot() . '" && composer require wp-cli/wp-cli\'' . PHP_EOL;
     exit(1);
   }
 }
 
-echo 'The Drush launcher could not find a Drupal site to operate on. Please do *one* of the following:' . PHP_EOL;
-echo '  - Navigate to any where within your Drupal project and try again.' . PHP_EOL;
-echo '  - Add --root=/path/to/drupal so Drush knows where your site is located.' . PHP_EOL;
+echo 'The WP-CLI Launcher could not find a WordPress site to operate on. Please do *one* of the following:' . PHP_EOL;
+echo '  - Navigate to any where within your WordPress project and try again.' . PHP_EOL;
+echo '  - Add --path=path/to/wordpress so WP-CLI knows where your site is located.' . PHP_EOL;
 exit(1);
